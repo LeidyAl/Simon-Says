38d726d303c0fd38a8a07281180d19e531053251
LeidyAl <leidyalvarez20412@gmail.com>
2021-03-06 17:29:41 -0500

    Final details

--- code.js
+++ code.js
@@ -7,15 +7,10 @@ const LastLevel = 10
 
 class Game {
     constructor() {
+        this.InitGame = this.InitGame.bind(this);
         this.InitGame();
         this.SequenceGenerator();
         setTimeout(this.nextLevel, 500)
-    }
-
-    InitGame() {
-        this.nextLevel = this.nextLevel.bind(this)
-        btnStart.classList.add('hide');
-        this.level = 1
         this.colors = {
             green,
             purple,
@@ -24,12 +19,30 @@ class Game {
         }
     }
 
+    InitGame() {
+        this.nextLevel = this.nextLevel.bind(this)
+        this.chooseColor = this.chooseColor.bind(this);
+        this.RemoveEventClick = this.RemoveEventClick.bind(this);
+        this.toggleBtnStart();
+        this.level = 1
+        
+    }
+
+    toggleBtnStart(){
+        if(btnStart.classList.contains('hide')){
+            btnStart.classList.remove('hide');
+        }
+        else{
+            btnStart.classList.add('hide');
+            
+        }
+    }
     SequenceGenerator() {
         this.sequence = new Array(LastLevel).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 
     nextLevel() {
-        this.subNivel = 0;
+        this.subLevel = 0;
         this.LigthSequence();
         this.AddEventClick();
     }
@@ -76,40 +89,52 @@ class Game {
         this.colors[color].classList.remove('light')
     }
     AddEventClick() {
-        this.colors.green.addEventListener('click', this.chooseColor.bind(this))
-        this.colors.purple.addEventListener('click', this.chooseColor.bind(this))
-        this.colors.blue.addEventListener('click', this.chooseColor.bind(this))
-        this.colors.yellow.addEventListener('click', this.chooseColor.bind(this))
+        this.colors.green.addEventListener('click', this.chooseColor)
+        this.colors.purple.addEventListener('click', this.chooseColor)
+        this.colors.blue.addEventListener('click', this.chooseColor)
+        this.colors.yellow.addEventListener('click', this.chooseColor)
     }
 
     RemoveEventClick() {
-        this.colors.green.removeEventListener('click', this.chooseColor.bind(this))
-        this.colors.purple.removeEventListener('click', this.chooseColor.bind(this))
-        this.colors.blue.removeEventListener('click', this.chooseColor.bind(this))
-        this.colors.yellow.removeEventListener('click', this.chooseColor.bind(this))
+        this.colors.green.removeEventListener('click', this.chooseColor)
+        this.colors.purple.removeEventListener('click', this.chooseColor)
+        this.colors.blue.removeEventListener('click', this.chooseColor)
+        this.colors.yellow.removeEventListener('click', this.chooseColor)
     }
 
     chooseColor(e) {
         const nameColor = e.target.dataset.color;
         const numberColor = this.ColorToNumber(nameColor);
         this.ToIlluminateColor(nameColor);
-        if (numberColor === this.sequence[this.subNivel]) {
-            this.subNivel++;
-            if (this.subNivel === this.level) {
+        if (numberColor === this.sequence[this.subLevel]) {
+            this.subLevel++;
+            if (this.subLevel === this.level) {
                 this.level++;
                 this.RemoveEventClick();
                 if (this.level === (LastLevel + 1)) {
-                    console.log("Ganaste")
+                    this.WinGame();
                 }
                 else {
                     setTimeout(this.nextLevel, 1500);
                 }
-            }
+            } 
         }
         else {
-            console.log("Perdiste");
+            this.LoseGame();
         }
+       
+    }
+    WinGame() {
+        swal('¡Felicidades!', 'Ganaste el juego', 'success')
+            .then(this.InitGame.bind(this));
+    }
 
+    LoseGame() {
+        swal('¡Lo lamento!', 'Perdiste el juego :(', 'error')
+            .then(() => {
+                this.RemoveEventClick();
+                this.InitGame();
+            });
     }
 }
 
